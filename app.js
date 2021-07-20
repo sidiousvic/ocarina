3fa5d0efebd0140ee47151a803053344baaf5d34
sidiousvic <sidiousvic@gmail.com>
2021-07-21 01:11:16 +0900

    Add mouse interaction

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-window.addEventListener('keydown', e => {
+window.addEventListener("keydown", (e) => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
   if (!audio) return;
-  key.classList.add('playing');
+  key.classList.add("playing");
   audio.currentTime = 0;
   audio.play();
 });
 
-window.addEventListener('keyup', e => {
+window.addEventListener("mousedown", (e) => {
+  const audio = document.querySelector(
+    `audio[data-key="${e.target.getAttribute("data-key")}"]`
+  );
+
+  const key = e.target;
+
+  if (!audio) return;
+  if (key) {
+    key.classList.add("playing");
+    audio.currentTime = 0;
+    audio.play();
+  }
+});
+
+window.addEventListener("keyup", (e) => {
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-  key.classList.remove('playing');
+  key.classList.remove("playing");
+});
+
+window.addEventListener("mouseup", (e) => {
+  e.target.classList.remove("playing");
 });
